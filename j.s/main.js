78697e38685ff84cc40899a9eff0adf029ef5eae
marwanabubakr <dev@marobakr>
2023-08-03 03:43:53 +0300

    scss

---
+++ j.s/main.js
@@ -0,0 +1,6 @@
+// document.querySelector('.the-toggler').onclick = function () {
+//   document.querySelector('.navbar').style = [`display:block`];
+// };
+// document.querySelector('.the-toggler').onblur = function () {
+//   document.querySelector('.navbar').style = [`display:none`];
+// };
